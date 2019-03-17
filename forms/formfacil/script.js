b790cb86c171c3b5bc9acc4f57fcf36ab40cf378
anthonyviquez <48371396+anthonyviquez@users.noreply.github.com>
2019-03-16 21:48:07 -0600

    Add files via upload

---
+++ forms/formfacil/script.js
@@ -0,0 +1,22 @@
+(function () {
+    var FACIL = {
+        init: function () {
+            this.addEvents();
+        },
+
+        addEvents: function(){
+            $('#form-login').on('submit', function(event){
+                //detengo el envio del form
+                event.preventDefault();
+
+                console.log('detengo el envio del form');
+                
+                console.log( 'data', $('#form-login').serialize() + '&total=3000$' );
+
+                //tambien hacemos uso de ajax
+            });
+        }
+    }
+
+    FACIL.init();
+})();
